f094f28357f9728dda54ed138a5225df34d7227f
halcos16 <alexcardona.majo@gmail.com>
2024-07-22 14:15:05 -0500

    carpeta lista de tareas

--- 03-dinamic-dom/app.js
+++ 03-dinamic-dom/app.js
@@ -1,18 +1,41 @@
 const listaColores = document.querySelector('#listaColores');
 const btnCrear = document.querySelector('#btnCrear');
 const main = document.querySelector('#main');
+const btnBorrar = document.querySelector('#btnBorrar');
+let n = 1;
 
-function crear () {
-   
+
+function crear (){
+    
     const ball = document.createElement('div');
-    ball.innerText = "pelotica";
-    ball.classList.add('pelotica')
+    const equis = document.createElement('button');
+
+    equis.innerText = 'x';
+    equis.classList.add('equis');
+
+    ball.classList.add('pelotica');
     ball.style.backgroundColor = listaColores.value;
+    ball.innerText = n++;
+/*     ball.innerHTML = equis; */
+    ball.appendChild(equis)
 
-    main.appendChild(ball)
+    main.appendChild(ball);
+}
 
-    console.log(ball)
-
+function borrarPantalla (){
+    main.innerHTML = '';
+    n=1;
+}
+function borrarElemento (e){ 
+    if(e.target.innerText == 'x'){
+        main.removeChild(e.target.parentElement);
+    }
+}
+function completarTarea (e){ 
+    e.target.style.textDecoration = 'line-through';
 }
 
 btnCrear.addEventListener('click',crear)
+btnBorrar.addEventListener('click',borrarPantalla)
+main.addEventListener('click',borrarElemento)
+main.addEventListener('click',completarTarea)
