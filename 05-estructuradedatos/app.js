e7b7de97df6198c3dc3536777a13764eb4cb5d7e
halcos16 <alexcardona.majo@gmail.com>
2024-07-25 15:37:29 -0500

    structura

---
+++ 05-estructuradedatos/app.js
@@ -0,0 +1,22 @@
+/* id, nombre, year, genero, image*/
+let peliculas = [
+    {
+        id: 1,
+        nombre: 'Twisters',
+        year: 2024,
+        direccion: 'Lee Isaac Chung',
+        guionistas: ['Mark L. Smith','Michael Crichton','Joseph Kosinski'],
+        genero: 'acción',
+        image: 'https://upload.wikimedia.org/wikipedia/en/2/24/Twisters_Official_US_Theatrical_Poster.jpg'
+    },
+    {
+        id: 1,
+        nombre: 'Twisters',
+        year: 2024,
+        direccion: 'Lee Isaac Chung',
+        guionistas: ['Mark L. Smith','Michael Crichton','Joseph Kosinski'],
+        genero: 'acción',
+        image: 'https://upload.wikimedia.org/wikipedia/en/2/24/Twisters_Official_US_Theatrical_Poster.jpg'
+    }
+]
+
---
+++ 05-estructuradedatos/bd.js
@@ -0,0 +1,31 @@
+/* id, nombre, year, genero, image*/
+let peliculas = [
+    {
+        id: 1,
+        nombre: 'El padrino',
+        year: 1984,
+        direccion: 'Francis Ford Coppola',
+        protagonistas: ['Marlon Brando', 'Al Pacino', 'James Caan'],
+        genero: ['Crimen', 'Drama'],
+        image: 'https://es.wikipedia.org/wiki/El_padrino_%28pel%C3%ADcula%29#/media/Archivo:The_Godfather_logo.svg'
+    },
+    {
+        id: 2,
+        nombre: 'Cazadores del Arca Perdida',
+        year: 1981,
+        direccion: 'Steven Spielberg',
+        Protagonistas: ['Harrison Ford', 'Karen Allen', 'Paul Freeman'],
+        genero: ['acción', 'aventura'],
+        image: 'https://upload.wikimedia.org/wikipedia/en/2/24/Twisters_Official_US_Theatrical_Poster.jpg'
+    },
+    {
+        id: 3,
+        nombre: 'La redencion de Shawshank',
+        year: 1994,
+        direccion: 'Frank Darabont',
+        Protagonistas: ['Tim Robbins', 'Morgan Freeman'],
+        genero: 'Drama',
+        image: 'https://upload.wikimedia.org/wikipedia/en/2/24/Twisters_Official_US_Theatrical_Poster.jpg'
+    },
+]
+
